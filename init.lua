fe6977ccca9a53bf0743064ac147987a200fcba6
Roberto Esteves <robertoesteves@protonmail.com>
2021-10-18 22:51:52 -0300

    fix identation

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@ require('plugins')
 
 vim.o.background = "dark"
 vim.o.termguicolors = true
-vim.api.nvim_set_var('gruvbox_contrast_dark', 'hard') vim.api.nvim_set_var('gruvbox_invert_selection', '0')
+vim.api.nvim_set_var('gruvbox_contrast_dark', 'hard')
+vim.api.nvim_set_var('gruvbox_invert_selection', '0')
 
 vim.api.nvim_set_var('&t_8f', '<Esc>[38;2;%lu;%lu;%lum')
 vim.api.nvim_set_var('&t_8f', '<Esc>[48;2;%lu;%lu;%lum')
