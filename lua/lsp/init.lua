b8bea00a2367085260e6b09215653b93f1ff2b8e
Roberto Esteves <robertoesteves@protonmail.com>
2021-12-14 16:51:36 -0300

    fix warnings

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -38,6 +38,16 @@ return function ()
         capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
       }
 
+      if server.name == 'sumneko_lua' then
+        opts.settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
+      end
+
       server:setup(opts)
       vim.cmd [[ do User LspAttachBuffers ]]
   end)
