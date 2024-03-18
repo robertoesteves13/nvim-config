aa628114c5e099ff5fcbb03728c15cf5d8a9cf9e
Roberto Esteves <contact@robertoesteves.dev>
2024-03-18 12:24:23 -0300

    update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,7 +15,7 @@ return function()
   local plugins = {
     -- Colorschemes
     { 'folke/tokyonight.nvim',    lazy = true },
-    { "ellisonleao/gruvbox.nvim", lazy = true },
+    { "ellisonleao/gruvbox.nvim", priority = 1000,     opts = {} },
     { "catppuccin/nvim",          name = 'catppuccin', lazy = true },
     { "jacoborus/tender.vim",     lazy = true },
     { "savq/melange-nvim",        lazy = true },
