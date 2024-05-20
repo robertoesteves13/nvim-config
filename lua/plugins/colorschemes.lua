5f95462beb42275a35b7137bb065ce9de8fdfa2a
Roberto Esteves <contact@robertoesteves.dev>
2024-05-20 13:42:07 -0300

    fix: colorscheme on inlay hints

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,7 +1,17 @@
 return {
-	{ "folke/tokyonight.nvim", lazy = true },
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, opts = {} },
-	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
-	{ "jacoborus/tender.vim", lazy = true },
-	{ "savq/melange-nvim", lazy = true },
+  { "folke/tokyonight.nvim", lazy = true },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      overrides = {
+        LspInlayHint = {
+          bg = "#504945",
+        }
+      }
+    }
+  },
+  { "catppuccin/nvim",       name = "catppuccin", lazy = true },
+  { "jacoborus/tender.vim",  lazy = true },
+  { "savq/melange-nvim",     lazy = true },
 }
