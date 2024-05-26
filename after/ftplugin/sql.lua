78a8143b84ccc4f7c00f91e5258390024a0ebd03
Roberto Esteves <contact@robertoesteves.dev>
2024-05-26 10:16:21 -0300

    feat(sql): Add completion via dadbod

--- after/ftplugin/sql.lua
+++ after/ftplugin/sql.lua
@@ -1,2 +1,10 @@
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
+
+local cmp = require("cmp")
+cmp.setup.filetype("sql", {
+	sources = cmp.config.sources({
+		{ name = "vim-dadbod-completion" },
+		{ name = "buffer" },
+	}),
+})
