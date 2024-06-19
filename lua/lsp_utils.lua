8a9ec1004e543375ad75379ce07508399548761c
Roberto Esteves <contact@robertoesteves.dev>
2024-06-19 16:11:42 -0300

    fix(lsp): correctly register client capabilities

--- lua/lsp_utils.lua
+++ lua/lsp_utils.lua
@@ -31,9 +31,15 @@ local function SetupLsp(opts)
 		o.settings = opts.settings
 	end
 
+	local capabilities = vim.tbl_deep_extend(
+		"force",
+		vim.lsp.protocol.make_client_capabilities(),
+		require("cmp_nvim_lsp").default_capabilities()
+	)
 	if opts["capabilities"] ~= nil then
-		o.capabilities = opts.capabilities
+		capabilities = vim.tbl_deep_extend("force", capabilities, opts.capabilities)
 	end
+	o.capabilities = capabilities
 
 	local client = vim.lsp.start(o)
 
