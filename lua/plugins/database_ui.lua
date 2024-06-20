50a86de070eb60d9bedc9b4fc4f962ed5c702c4a
Roberto Esteves <contact@robertoesteves.dev>
2024-06-20 07:44:03 -0300

    fix: configure db completion correctly

--- lua/plugins/database_ui.lua
+++ lua/plugins/database_ui.lua
@@ -1,9 +1,9 @@
 return {
+	{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
 	{
 		"kristijanhusak/vim-dadbod-ui",
 		dependencies = {
 			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
 		},
 		cmd = {
 			"DBUI",
